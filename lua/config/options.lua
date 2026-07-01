56a5d08f164de8a39e3a0253097fda538acdfeb5
wolewnicki <wjolewnicki@gmail.com>
2026-07-01 14:38:50 -0400

    commit before fedora go bye-bye

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.autoformat = false
+vim.g.autoformat = true
 vim.opt.colorcolumn = "80,100"
 
 vim.api.nvim_create_autocmd("FileType", {
